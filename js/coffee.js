a04cd7840e87ca503d1b210b0976b8d1c65eeb2c
mjj3127 <200594567@student.georgianc.on.ca>
2026-07-20 02:29:57 -0400

    coffee.js

--- js/coffee.js
+++ js/coffee.js
@@ -9,7 +9,6 @@ class Coffee {
     this.isDecaf = isDecaf;
   }
 
-  /* Parent class method */
   serveIt() {
     output.innerHTML += `Here is your ${this.size} ${this.isDecaf ? "Decaf" : "Regular"} coffee.<br>`;
   }
@@ -18,57 +17,43 @@ class Coffee {
 /* STEP 2: Create a Coffee object */
 const coffee1 = new Coffee("Medium", false);
 
-/* STEP 3 & 4: Call the inherited method */
+/* STEP 3: Call the parent method */
 coffee1.serveIt();
 
-/* STEP 5: Create a subclass */
+/* STEP 4: Create a subclass */
 class Mocha extends Coffee {
   constructor(size, isDecaf, shots, flavour) {
-    // Call the parent constructor
     super(size, isDecaf);
 
-    // Unique subclass properties
     this.shots = shots;
     this.flavour = flavour;
   }
 
-  /* Custom subclass method */
   mochaDesc() {
-    const coffeeType = this.isDecaf ? "decaf" : "regular";
-
-    return `A ${this.size} ${coffeeType} Mocha with ${this.shots} espresso shot(s) and ${this.flavour} syrup.`;
+    return `A ${this.size} ${this.isDecaf ? "Decaf" : "Regular"} Mocha with ${this.shots} espresso shot(s) and ${this.flavour} syrup.`;
   }
 }
 
-/* STEP 6: Create two Mocha objects */
-
+/* STEP 5: Create objects */
 const mocha1 = new Mocha("Large", false, 2, "Caramel");
-
 const mocha2 = new Mocha("Small", true, 1, "Vanilla");
 
-/* STEP 7: Display the results */
-
+/* STEP 6: Display */
 mocha1.serveIt();
 output.innerHTML += mocha1.mochaDesc() + "<br><br>";
 
 mocha2.serveIt();
 output.innerHTML += mocha2.mochaDesc() + "<br><br>";
 
-/* STEP 8:
-Create another object and test it.
-This can also be satisfied the console exercise if we want
-to try the methods there.
-*/
-
+/* STEP 7: Third object */
 const mocha3 = new Mocha("Medium", false, 3, "Hazelnut");
 
 mocha3.serveIt();
 output.innerHTML += mocha3.mochaDesc() + "<br><br>";
 
 /*
-It  can also be tested these in the browser console:
+Console test:
 
 mocha3.serveIt();
 console.log(mocha3.mochaDesc());
-
 */
