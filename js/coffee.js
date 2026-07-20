1174f006a69a70a88c44f09c2887f18b8432eed3
mjj3127 <200594567@student.georgianc.on.ca>
2026-07-20 02:38:46 -0400

    coffee.js

--- js/coffee.js
+++ js/coffee.js
@@ -2,7 +2,7 @@
 
 const output = document.querySelector("#output");
 
-/* STEP 1: Coffee class */
+/* Parent Coffee class */
 class Coffee {
   constructor(size, isDecaf) {
     this.size = size;
@@ -10,17 +10,14 @@ class Coffee {
   }
 
   serveIt() {
-    output.innerHTML += `Here is your ${this.size} ${this.isDecaf ? "Decaf" : "Regular"} coffee.<br>`;
+    output.innerHTML += `
+      Here is your ${this.size}
+      ${this.isDecaf ? "Decaf" : "Regular"} coffee.<br>
+    `;
   }
 }
 
-/* STEP 2: Create a Coffee object */
-const coffee1 = new Coffee("Medium", false);
-
-/* STEP 3: Call the parent method */
-coffee1.serveIt();
-
-/* STEP 4: Create a subclass */
+/* Mocha subclass */
 class Mocha extends Coffee {
   constructor(size, isDecaf, shots, flavour) {
     super(size, isDecaf);
@@ -30,30 +27,19 @@ class Mocha extends Coffee {
   }
 
   mochaDesc() {
-    return `A ${this.size} ${this.isDecaf ? "Decaf" : "Regular"} Mocha with ${this.shots} espresso shot(s) and ${this.flavour} syrup.`;
+    return `A ${this.size} ${
+      this.isDecaf ? "decaf" : "regular"
+    } Mocha with ${this.shots} shot(s) and ${this.flavour} syrup.`;
   }
 }
 
-/* STEP 5: Create objects */
+/* Create at least two Mocha objects */
 const mocha1 = new Mocha("Large", false, 2, "Caramel");
 const mocha2 = new Mocha("Small", true, 1, "Vanilla");
 
-/* STEP 6: Display */
+/* Call inherited and custom methods */
 mocha1.serveIt();
-output.innerHTML += mocha1.mochaDesc() + "<br><br>";
+output.innerHTML += `${mocha1.mochaDesc()}<br><br>`;
 
 mocha2.serveIt();
-output.innerHTML += mocha2.mochaDesc() + "<br><br>";
-
-/* STEP 7: Third object */
-const mocha3 = new Mocha("Medium", false, 3, "Hazelnut");
-
-mocha3.serveIt();
-output.innerHTML += mocha3.mochaDesc() + "<br><br>";
-
-/*
-Console test:
-
-mocha3.serveIt();
-console.log(mocha3.mochaDesc());
-*/
+output.innerHTML += `${mocha2.mochaDesc()}<br><br>`;
