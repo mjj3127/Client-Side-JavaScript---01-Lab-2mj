711bc7dbb578fa20311c23908edc46ae977e599a
mjj3127 <200594567@student.georgianc.on.ca>
2026-07-20 02:44:52 -0400

    coffee.js

--- js/coffee.js
+++ js/coffee.js
@@ -2,22 +2,26 @@
 
 const output = document.querySelector("#output");
 
-/* Parent Coffee class */
+/* STEP 1: Parent Coffee class */
 class Coffee {
   constructor(size, isDecaf) {
     this.size = size;
     this.isDecaf = isDecaf;
   }
 
+  /* Parent class method */
   serveIt() {
-    output.innerHTML += `
-      Here is your ${this.size}
-      ${this.isDecaf ? "Decaf" : "Regular"} coffee.<br>
-    `;
+    output.innerHTML += `Here is your ${this.size} ${this.isDecaf ? "Decaf" : "Regular"} coffee.<br>`;
   }
 }
 
-/* Mocha subclass */
+/* STEP 2: Create a Coffee object */
+const coffee1 = new Coffee("Medium", false);
+
+/* STEP 3: Call the parent method */
+coffee1.serveIt();
+
+/* STEP 4: Create a subclass */
 class Mocha extends Coffee {
   constructor(size, isDecaf, shots, flavour) {
     super(size, isDecaf);
@@ -26,20 +30,32 @@ class Mocha extends Coffee {
     this.flavour = flavour;
   }
 
+  /* Custom subclass method */
   mochaDesc() {
-    return `A ${this.size} ${
-      this.isDecaf ? "decaf" : "regular"
-    } Mocha with ${this.shots} shot(s) and ${this.flavour} syrup.`;
+    return `A ${this.size} ${this.isDecaf ? "Decaf" : "Regular"} Mocha with ${this.shots} espresso shot(s) and ${this.flavour} syrup.`;
   }
 }
 
-/* Create at least two Mocha objects */
+/* STEP 5: Create at least two Mocha objects */
 const mocha1 = new Mocha("Large", false, 2, "Caramel");
 const mocha2 = new Mocha("Small", true, 1, "Vanilla");
 
-/* Call inherited and custom methods */
+/* STEP 6: Call inherited method and custom method */
 mocha1.serveIt();
-output.innerHTML += `${mocha1.mochaDesc()}<br><br>`;
+output.innerHTML += mocha1.mochaDesc() + "<br><br>";
 
 mocha2.serveIt();
-output.innerHTML += `${mocha2.mochaDesc()}<br><br>`;
+output.innerHTML += mocha2.mochaDesc() + "<br><br>";
+
+/* STEP 7: Create another object for testing */
+const mocha3 = new Mocha("Medium", false, 3, "Hazelnut");
+
+mocha3.serveIt();
+output.innerHTML += mocha3.mochaDesc() + "<br><br>";
+
+/*
+Console Testing
+
+mocha3.serveIt();
+console.log(mocha3.mochaDesc());
+*/
